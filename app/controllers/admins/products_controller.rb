6be9f222d8f263fd7e00ef21a4f2a83518de5214
Bouncyyahomie <thornny.s@gmail.com>
2021-04-18 23:34:10 +0700

    fixed update

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -48,7 +48,7 @@ module Admins
     def update
       respond_to do |format|
         if @product.update(product_params)
-          format.html { redirect_to @product, notice: "Product was successfully updated." }
+          format.html { redirect_to admins_product_path(@product), notice: "Product was successfully updated." }
           format.json { render :show, status: :ok, location: @product }
         else
           format.html { render :edit, status: :unprocessable_entity }
