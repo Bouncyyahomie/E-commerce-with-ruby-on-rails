d7348cda7c7de308e19bad9fcd0a73fc692c956e
Bouncyyahomie <thornny.s@gmail.com>
2021-04-26 21:43:08 +0700

    set admin mode

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   
   devise_for :admins
   match "/admins/products/delete_image/:id" => "admins/products#delete_image", via: :get
-  
   namespace :admins do
     namespace :products do
       post 'csv_upload'
